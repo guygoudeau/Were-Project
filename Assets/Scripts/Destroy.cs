bfa5de770a65e0541db7d7cdbc388214d37f47d3
atouchetAIE <cidleaf@gmail.com>
2016-02-16 16:37:40 -0600

    Fixes

---
+++ Assets/Scripts/Destroy.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Destroy : MonoBehaviour {
+
+    public float lifetime;
+
+	// Use this for initialization
+	void Start () {
+        DestroyObject(gameObject, lifetime);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Assets/Scripts/WinCondition.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class WinCondition : MonoBehaviour {
+
+    private GameObject info;
+
+	void Start () {
+        info = GameObject.Find("InformationHolder(Clone)");
+	}
+
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        if (info.GetComponent<InfoHandler>().req[0] == true)
+        {
+            if (info.GetComponent<InfoHandler>().req[1] == true)
+            {
+                if (info.GetComponent<InfoHandler>().req[2] == true)
+                {
+                    SceneManager.LoadScene("Win");
+                }
+            }
+        }
+    }
+
+}
